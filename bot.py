9d860898d90ea9dc2fe9f79252f52fb288cb9a39
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-01 15:08:11 +0200

    vcmute cmd add

--- bot.py
+++ bot.py
@@ -121,6 +121,17 @@ async def join(ctx):
 async def leave(ctx):
     await ctx.voice_client.disconnect()
 
+@client.command()
+async def vcmute(ctx):
+    vc = ctx.author.voice.channel
+    for member in vc.members:
+        await member.edit(mute=True)
+
+@client.command()
+async def vcunmute(ctx):
+    vc = ctx.author.voice.channel
+    for member in vc.members:
+        await member.edit(mute=False)
 # --------------------- fun commands --------------------------------------
 
 
