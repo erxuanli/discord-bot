fa4978ae93cb2db1985b80d44b61f9107897f0e4
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-04 16:48:01 +0200

    new code structure

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from cogs.cmds.general_cmds import GeneralCmds
 import discord
 from discord.ext import commands
 
@@ -11,6 +12,8 @@ import pi_num
 
 client = commands.Bot(command_prefix=".", help_command=None)
 
+client.add_cog(GeneralCmds(client))
+
 
 @client.event
 async def on_ready():
@@ -19,86 +22,7 @@ async def on_ready():
 
 
 # ---------------------- general commands -----------------------------------
-@client.command()
-async def creator(ctx):
-    await ctx.send("PolarBear4u sennnnpaiiiiii")
 
-
-@client.command()
-async def help(ctx):
-    embed = discord.Embed(title="help",
-                          description="all useless cmds are listed below",
-                          color=discord.Color.purple())
-    embed.set_author(name=ctx.author.display_name,
-                     icon_url=ctx.author.avatar_url)
-    embed.set_thumbnail(
-        url="https://static.wikia.nocookie.net/the-muse-list/images/f/fd/Albedo_Pout.jpg")
-    embed.add_field(name="**;help**",
-                    value="That's this command xD", inline=False)
-    embed.add_field(name="**;creator**",
-                    value="creator of this useless bot", inline=False)
-    embed.add_field(name="**;prefix**",
-                    value="returns the current prefix", inline=False)
-    embed.add_field(
-        name="**;avatar [user]**", value="returns the avatar of the mentioned user", inline=False)
-    embed.add_field(
-        name="**;clear [int]**", value="clears channel [max 30]; do not spam", inline=False)
-    embed.add_field(name="**;ping**", value="bot latency", inline=False)
-    embed.add_field(name="**;time**",
-                    value="returns the current time (of host location)", inline=False)
-    embed.add_field(name="**;join**",
-                    value="join vc", inline=False)
-    embed.add_field(name="**;leave**",
-                    value="leave vc", inline=False)
-    embed.add_field(name="**;vcmute**",
-                    value="mute all in vc", inline=False)
-    embed.add_field(name="**;vcunmute**",
-                    value="unmute all in vc", inline=False)
-    embed.add_field(name="**;quote**",
-                    value="returns random quote", inline=False)
-    embed.add_field(name="**;ascii**",
-                    value="converts to ascii / unicode", inline=False)
-    embed.add_field(name="**;ranimegif**",
-                    value="random anime gif", inline=False)
-    embed.add_field(name="**;morse**",
-                    value="converts to morse code", inline=False)
-    embed.add_field(name="**;demorse**",
-                    value="converts morse code back", inline=False)
-    embed.add_field(name="**;sqfunc**",
-                    value="returns zero points of a quadratic function", inline=False)
-    embed.add_field(name="**;pi**", value="returns pi", inline=False)
-    embed.add_field(name="**;dice [min] [max]**",
-                    value="random number", inline=False)
-    embed.add_field(name="**;sheeesh**",
-                    value="completely useless cmd", inline=False)
-    embed.add_field(name="**;melih**",
-                    value="completely useless cmd", inline=False)
-    embed.set_footer(
-        text="Thanks for using this useless bot xD and greetings to all weebs xD")
-
-    await ctx.send(embed=embed)
-
-
-@client.command()
-async def prefix(ctx):
-    await ctx.send(client.command_prefix)
-
-
-@client.command()
-async def avatar(ctx, user: discord.Member = None):
-    if user is None:
-        await ctx.send(ctx.author.avatar_url)
-    else:
-        userAvatarUrl = user.avatar_url
-        await ctx.send(userAvatarUrl)
-
-
-@client.command()
-async def clear(ctx, amount=5):
-    if amount > 30:
-        await ctx.send("max amount 30")
-    else:
-        await ctx.channel.purge(limit=amount)
 
 # ---------------------- testing commands ----------------------------------
 
