2aa73cfe566b889ba9d9d3bad9808cbea184d719
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-06 17:34:31 +0200

    bugfix

--- cogs/cmds/other_cmds.py
+++ cogs/cmds/other_cmds.py
@@ -9,21 +9,23 @@ class OtherCmds(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.nicktimer_info = dict()
-        self.test_nick_change.start()
+        self.refresh_nicktimers.start()
 
     @commands.command()
     async def nicktimer(self, ctx, timer : int):
+        self.nicktimer_info[str(ctx.guild.id)][str(ctx.author.id)]["ctx"] = ctx
         self.nicktimer_info[str(ctx.guild.id)][str(ctx.author.id)]["start"] = time.time()
-        self.nicktimer_info[str(ctx.guild.id)][str(ctx.author.id)]["timer"] = timer
+        self.nicktimer_info[str(ctx.guild.id)][str(ctx.author.id)]["end"] = self.nicktimer_info[str(ctx.guild.id)][str(ctx.author.id)]["start"] + timer
 
     @tasks.loop(seconds = 10)
     async def refresh_nicktimers(self):
-        pass
+        for server in self.nicktimer_info.keys():
+            for user in self.nicktimer_info[server]:
+                ctx = self.nicktimer_info[server][user]["ctx"]
+                start = self.nicktimer_info[server][user]["start"]
+                end = self.nicktimer_info[server][user]["end"]
+                await ctx.author.edit(nick = str(end - start))
 
-    @tasks.loop(seconds = 10)
-    async def test_nick_change(self):
-        user = self.client.get_user(172002275412279296)
-        await user.edit(nick = "test")
 
 
 
