4833f8a569a9252a75906f4fc076a9c02a3bc551
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-06 22:32:47 +0200

    info cmd

--- cogs/cmds/general_cmds.py
+++ cogs/cmds/general_cmds.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 import os
+import datetime
 
 from cogs.cmds.custom_checks import is_creator
 
@@ -33,10 +34,32 @@ class GeneralCmds(commands.Cog):
         else:
             await ctx.channel.purge(limit=amount)
 
-
     @commands.command()
     async def id(self, ctx, member : discord.Member = None):
         if member is None:
             await ctx.send(str(ctx.author.id))
         else:
             await ctx.send(str(member.id))
+
+    @commands.command()
+    async def info(self, ctx, member : discord.Member = None):
+        user = member
+        if user is None:
+            user = ctx.author
+
+        embed = discord.Embed(title = f"User Info [{str(user)} || {str(user.id)}]", color = discord.Color.red())
+
+        fields = [("User Tag", str(user), True),
+                  ("User ID", str(user.id), True),
+                  ("Created at", user.created_at.strftime("%d.%m.%Y || %H:%M:&S"), True),
+                  ("Joined at", user.joined_at.strftime("%d.%m.%Y || %H:%M:&S"), True),
+                  ("Highest Role", user.top_role.mention, True),
+                  ("Status", str(user.status), True)]
+
+        for name, value, inline in fields:
+            embed.add_field(name = name, value = value, inline = inline)
+
+        embed.set_footer(text=f"Time (bot host location): {datetime.datetime.now()}")
+
+        await ctx.send(embed = embed)
+        
