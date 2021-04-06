db5327860dc06a1bfc19f6ac0c6bcf5c54180109
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-06 18:25:11 +0200

    nicktimer comment otu

--- cogs/cmds/other_cmds.py
+++ cogs/cmds/other_cmds.py
@@ -9,24 +9,24 @@ import time
 class OtherCmds(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.nicktimer_info = dict()
-        self.refresh_nicktimers.start()
+        # self.nicktimer_info = dict()
+        # self.refresh_nicktimers.start()
 
-    @commands.command()
-    async def nicktimer(self, ctx, timer: int):
-        start_time = time.time()
-        self.nicktimer_info[str(ctx.author.id)] = {
-            "ctx": ctx, "start": start_time, "end": start_time + timer}
+    # @commands.command()
+    # async def nicktimer(self, ctx, timer: int):
+    #     start_time = time.time()
+    #     self.nicktimer_info[str(ctx.author.id)] = {
+    #         "ctx": ctx, "start": start_time, "end": start_time + timer}
 
-    @commands.command()
-    async def print_dict(self, ctx):
-        await ctx.send(f"{self.nicktimer_info}")
+    # @commands.command()
+    # async def print_dict(self, ctx):
+    #     await ctx.send(f"{self.nicktimer_info}")
 
-    @tasks.loop(seconds=10)
-    async def refresh_nicktimers(self):
-        print("test loop")
-        # for user in self.nicktimer_info.keys():
-        #     ctx = self.nicktimer_info[user]["ctx"]
-        #     start_time = self.nicktimer_info[user]["start"]
-        #     end_time = self.nicktimer_info[user]["end"]
-        #     await ctx.author.edit(nick = str(end_time - start_time))
+    # @tasks.loop(seconds=10)
+    # async def refresh_nicktimers(self):
+    #     print("test loop")
+    #     # for user in self.nicktimer_info.keys():
+    #     #     ctx = self.nicktimer_info[user]["ctx"]
+    #     #     start_time = self.nicktimer_info[user]["start"]
+    #     #     end_time = self.nicktimer_info[user]["end"]
+    #     #     await ctx.author.edit(nick = str(end_time - start_time))
