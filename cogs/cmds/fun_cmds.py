7e87d99ee2c96b508b730c01995bfe0fc6e50725
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-07 18:45:18 +0200

    bugfix

--- cogs/cmds/fun_cmds.py
+++ cogs/cmds/fun_cmds.py
@@ -14,44 +14,24 @@ class FunCmds(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def satellite(self, ctx, quan : int = 1, *specific):
-        if specific is None and quan != 0:
-            count = 0
-            with open("./cogs/cmds/cmd_utils/sgp4/active_satellites.json", "r") as file:
-                satellites = json.load(file)
-                for satellite in satellites:
-                    if count >= quan:
-                        return
-                    s = satellites[satellite]["s"]
-                    t = satellites[satellite]["t"] 
-                    satellite_object = Satrec.twoline2rv(s, t)
-                    jd, fr = julian.to_jd(datetime.datetime.now(), fmt = "jd"), 0.0
-                    e, r, v = satellite_object.sgp4(jd, fr)
-                    embed = discord.Embed(title = f"[{satellite}]", color = discord.Color.gold())
-                    embed.add_field(name = "**True Equator Mean Equinox position (km)**", value = r, inline = False)
-                    embed.add_field(name = "**True Equator Mean Equinox velocity (km/s)**", value = v, inline = False)
-                    embed.set_footer(text = f"Julian Date [now]: [{jd}] || Fraction: [{fr}]")
-                    await ctx.send(embed = embed)
-                    count += 1
-        elif specific is not None and quan == 0:
-            with open("./cogs/cmds/cmd_utils/sgp4/active_satellites.json", "r") as file:
-                satellites = json.load(file)
-                for sat in specific:
-                    if satellites.get(sat, None) is None:
-                        await ctx.send(f"[{sat}] not found. Syntax Error or [{sat}] not in Database")
-                        continue
-                    else:
-                        s = satellites[sat]["s"]
-                        t = satellites[sat]["t"]
-                        satellite_object = Satrec.twoline2rv(s, t)
-                        jd, fr = julian.to_jd(datetime.datetime.now(), fmt = "jd"), 0.0
-                        e, r, v = satellite_object.sgp4(jd, fr)
-                        embed = discord.Embed(title = f"[{sat}]", color = discord.Color.gold())
-                        embed.add_field(name = "**True Equator Mean Equinox position (km)**", value = r, inline = False)
-                        embed.add_field(name = "**True Equator Mean Equinox velocity (km/s)**", value = v, inline = False)
-                        embed.set_footer(text = f"Julian Date [now]: [{jd}] || Fraction: [{fr}]")
-                        await ctx.send(embed = embed)
-                        continue
+    async def satellite(self, ctx, quan : int = 1):
+        count = 0
+        with open("./cogs/cmds/cmd_utils/sgp4/active_satellites.json", "r") as file:
+            satellites = json.load(file)
+            for satellite in satellites:
+                if count >= quan:
+                    return
+                s = satellites[satellite]["s"]
+                t = satellites[satellite]["t"] 
+                satellite_object = Satrec.twoline2rv(s, t)
+                jd, fr = julian.to_jd(datetime.datetime.now(), fmt = "jd"), 0.0
+                e, r, v = satellite_object.sgp4(jd, fr)
+                embed = discord.Embed(title = f"[{satellite}]", color = discord.Color.gold())
+                embed.add_field(name = "**True Equator Mean Equinox position (km)**", value = r, inline = False)
+                embed.add_field(name = "**True Equator Mean Equinox velocity (km/s)**", value = v, inline = False)
+                embed.set_footer(text = f"Julian Date [now]: [{jd}] || Fraction: [{fr}]")
+                await ctx.send(embed = embed)
+                count += 1
 
     @commands.command()
     async def quote(self, ctx):
