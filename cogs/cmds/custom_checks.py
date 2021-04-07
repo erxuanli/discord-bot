077a903b13ffacf1665b640d58161460a37f83ac
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-07 21:59:06 +0200

    troll flori

--- cogs/cmds/custom_checks.py
+++ cogs/cmds/custom_checks.py
@@ -3,4 +3,5 @@ import os
 def is_creator(ctx):
     return ctx.author.id == int(os.environ['MY_DISCORD_ID'])
 
-    
+def is_flori(ctx):
+    return ctx.author.id == 751084480097157251
--- cogs/non_cmds/troll_flori.py
+++ cogs/non_cmds/troll_flori.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from cogs.cmds.custom_checks import is_flori
 
 class TrollFlori(commands.Cog):
     def __init__(self, client):
@@ -8,18 +9,20 @@ class TrollFlori(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        if is_flori(message):
+            await message.channel.send("troll")
 
-        if message.content.startswith("mh"):
-            await message.channel.send("troll")
-        if message.content.startswith("ms"):
-            await message.channel.send("troll")
-        if message.content.startswith("mwork"):
-            await message.channel.send("troll")
-        if message.content.startswith("mcd"):
-            await message.channel.send("troll")
-        if message.content.startswith("mdaily"):
-            await message.channel.send("troll")
-        if message.content.startswith("mvote"):
-            await message.channel.send("troll")
-        if message.content.startswith("mbj"):
-            await message.channel.send("troll")
+        # if message.content.startswith("mh"):
+        #     await message.channel.send("troll")
+        # if message.content.startswith("ms"):
+        #     await message.channel.send("troll")
+        # if message.content.startswith("mwork"):
+        #     await message.channel.send("troll")
+        # if message.content.startswith("mcd"):
+        #     await message.channel.send("troll")
+        # if message.content.startswith("mdaily"):
+        #     await message.channel.send("troll")
+        # if message.content.startswith("mvote"):
+        #     await message.channel.send("troll")
+        # if message.content.startswith("mbj"):
+        #     await message.channel.send("troll")
