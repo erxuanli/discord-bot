c5fa90f2193d1e74f7396a62346611a9a74dcceb
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-07 19:22:43 +0200

    weather cmd

--- cogs/cmds/science_cmds.py
+++ cogs/cmds/science_cmds.py
@@ -6,6 +6,7 @@ import json
 from sgp4.api import Satrec 
 import julian
 import datetime
+import python_weather
 
 class ScienceCmds(commands.Cog):
     def __init__(self, client):
@@ -30,4 +31,18 @@ class ScienceCmds(commands.Cog):
                 embed.set_footer(text = f"Julian Date [now]: [{jd}] || Fraction: [{fr}]")
                 await ctx.send(embed = embed)
                 count += 1   
+
+    @commands.command()
+    async def weather(self, ctx, city : str = "Washington DC"):
+        weather_client = python_weather.Client(format = python_weather.IMPERIAL)
+        weather = await weather_client.find(city)
+
+        embed = discord.Embed(title = f"Weather [{city}]", color = discord.Color.orange())
+        embed.add_field(name = "Current City Temperature", value = weather.current.temperature, inline = False)
+
+        for forecast in weather.forecast:
+            embed.add_field(name = f"Date: [{forecast.date}]", value = f"Sky Text: {forecast.sky_text} || Temperature: {forecast.temperature}")
+
+        await weather_client.close()
+        await ctx.send(embed = embed)
                 
