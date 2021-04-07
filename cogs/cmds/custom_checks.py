3d59fffd1c800f6b327a3eca391ee37a38b50642
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-07 22:13:00 +0200

    is flori check update id

--- cogs/cmds/custom_checks.py
+++ cogs/cmds/custom_checks.py
@@ -4,4 +4,4 @@ def is_creator(ctx):
     return ctx.author.id == int(os.environ['MY_DISCORD_ID'])
 
 def is_flori(ctx):
-    return ctx.author.id == 751084480097157251
+    return ctx.author.id == 681969539935633441
--- cogs/non_cmds/troll_flori.py
+++ cogs/non_cmds/troll_flori.py
@@ -12,17 +12,3 @@ class TrollFlori(commands.Cog):
         if is_flori(message):
             await message.channel.send("troll")
 
-        # if message.content.startswith("mh"):
-        #     await message.channel.send("troll")
-        # if message.content.startswith("ms"):
-        #     await message.channel.send("troll")
-        # if message.content.startswith("mwork"):
-        #     await message.channel.send("troll")
-        # if message.content.startswith("mcd"):
-        #     await message.channel.send("troll")
-        # if message.content.startswith("mdaily"):
-        #     await message.channel.send("troll")
-        # if message.content.startswith("mvote"):
-        #     await message.channel.send("troll")
-        # if message.content.startswith("mbj"):
-        #     await message.channel.send("troll")
