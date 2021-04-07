ca43ac4abcf0deac7f7361b1e8b08eb77a86c428
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-07 18:50:18 +0200

    del unecessary

--- cogs/cmds/custom_checks.py
+++ cogs/cmds/custom_checks.py
@@ -1,9 +1,6 @@
 import os
 
 def is_creator(ctx):
-    # erxuan = int(os.environ['MY_DISCORD_ID'])
-    # melih = 237150657184923649 
-    # if melih == ctx.author.id or erxuan == ctx.author.id:
-    #     return True
-
     return ctx.author.id == int(os.environ['MY_DISCORD_ID'])
+
+    
