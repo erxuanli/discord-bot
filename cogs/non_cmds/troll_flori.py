2af5b157aa8d532dc10ae690a0bad95cba1dc663
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-07 22:32:00 +0200

    troll flori trigger

--- cogs/non_cmds/troll_flori.py
+++ cogs/non_cmds/troll_flori.py
@@ -6,9 +6,15 @@ from cogs.cmds.custom_checks import is_flori
 class TrollFlori(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.troll_flori_bool = True
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if is_flori(message):
+        if is_flori(message) and self.troll_flori_bool:
             await message.channel.send("troll")
+
+    @commands.command()
+    async def flori_troll(self, ctx):
+        self.troll_flori_bool = not self.troll_flori_bool
+        await ctx.send(f"Troll Flori: [{self.troll_flori_bool}]")
 
