e0d3894195362cfca6eb343064f3e5d1f78eaac1
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-16 23:27:28 +0200

    nickname timer command multi server

--- cogs/cmds/other_cmds.py
+++ cogs/cmds/other_cmds.py
@@ -15,13 +15,15 @@ class OtherCmds(commands.Cog):
     @commands.command()
     @commands.guild_only()
     async def nicktimer(self, ctx, t : int = 5): # time in minutes
-        if str(ctx.author.id) in self.nicktimer_data:
+        if str(ctx.author.id) in self.nicktimer_data[str(ctx.guild.id)]:
             await ctx.send("Already started a timer")
         else:
             if not self.nicktimer_refreshing:
                 if t <= 9_999_999_999_999:
                     end_time = time.time() + (t * 60)
-                    self.nicktimer_data[str(ctx.author.id)] = {"end_time": end_time, "ctx": ctx}
+                    if str(ctx.guild.id) not in self.nicktimer_data:
+                        self.nicktimer_data[str(ctx.guild.id)] = dict()
+                    self.nicktimer_data[str(ctx.guild.id)][str(ctx.author.id)] = {"end_time": end_time, "ctx": ctx}
                     await ctx.send(f"Timer set: {t} minutes")
                 else:
                     await ctx.send("Timer must be shorter than 9,999,999,999,999 minutes")
@@ -31,12 +33,12 @@ class OtherCmds(commands.Cog):
     @commands.command()
     @commands.guild_only()
     async def nicktimer_add(self, ctx, t : int = 1):
-        if str(ctx.author.id) not in self.nicktimer_data:
+        if str(ctx.author.id) not in self.nicktimer_data[str(ctx.guild.id)]:
             await ctx.send("You didn't set a timer")
         else:
             if not self.nicktimer_refreshing:
-                if (int((self.nicktimer_data[str(ctx.author.id)]["end_time"] - time.time()) / 60) + t) <= 9_999_999_999_999:
-                    self.nicktimer_data[str(ctx.author.id)]["end_time"] = self.nicktimer_data[str(ctx.author.id)]["end_time"] + (t * 60)
+                if (int((self.nicktimer_data[str(ctx.guild.id)][str(ctx.author.id)]["end_time"] - time.time()) / 60) + t) <= 9_999_999_999_999:
+                    self.nicktimer_data[str(ctx.guild.id)][str(ctx.author.id)]["end_time"] = self.nicktimer_data[str(ctx.guild.id)][str(ctx.author.id)]["end_time"] + (t * 60)
                     await ctx.send(f"Added {t} minutes to timer")
                 else:
                     await ctx.send("Timer must be shorter than 9,999,999,999,999 minutes")
@@ -46,12 +48,12 @@ class OtherCmds(commands.Cog):
     @commands.command()
     @commands.guild_only()
     async def nicktimer_stop(self, ctx):
-        if str(ctx.author.id) not in self.nicktimer_data:
+        if str(ctx.author.id) not in self.nicktimer_data[str(ctx.guild.id)]:
             await ctx.send("No timer running")
         else:
             if not self.nicktimer_refreshing:
                 try:
-                    del self.nicktimer_data[str(ctx.author.id)]
+                    del self.nicktimer_data[str(ctx.guild.id)][str(ctx.author.id)]
                     await ctx.author.edit(nick=ctx.author.name)
                     await ctx.send("Timer stopped")
                 except discord.errors.Forbidden:
@@ -64,22 +66,23 @@ class OtherCmds(commands.Cog):
         self.nicktimer_refreshing = True
         delete = []
         if self.nicktimer_data:
-            for user in self.nicktimer_data:
-                ctx = self.nicktimer_data[user]["ctx"]
-                end_time = self.nicktimer_data[user]["end_time"]
-                if time.time() >= end_time:
-                    try:
-                        await ctx.author.edit(nick=ctx.author.name)
-                        delete.append(user)
-                    except discord.errors.Forbidden:
-                        pass
-                else:
-                    try:
-                        await ctx.author.edit(nick=f"Back in {int((end_time - time.time()) / 60)} min {round((end_time - time.time()) - (int((end_time - time.time()) / 60) * 60))} sec")
-                    except discord.errors.Forbidden:
-                        pass
+            for server in self.nicktimer_data:
+                for user in server:
+                    ctx = self.nicktimer_data[server][user]["ctx"]
+                    end_time = self.nicktimer_data[server][user]["end_time"]
+                    if time.time() >= end_time:
+                        try:
+                            await ctx.author.edit(nick=ctx.author.name)
+                            delete.append(user)
+                        except discord.errors.Forbidden:
+                            pass
+                    else:
+                        try:
+                            await ctx.author.edit(nick=f"Back in {int((end_time - time.time()) / 60)} min {round((end_time - time.time()) - (int((end_time - time.time()) / 60) * 60))} sec")
+                        except discord.errors.Forbidden:
+                            pass
         for user in delete:
-            del self.nicktimer_data[user]
+            del self.nicktimer_data[server][user]
         delete = []
         self.nicktimer_refreshing = False
 
