6a5726e911b4ee8c4e021bd1f5f186fe35ecdc4c
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-22 21:00:35 +0200

    bugfix

--- cogs/error_handling/error_handling_cmds.py
+++ cogs/error_handling/error_handling_cmds.py
@@ -8,14 +8,16 @@ class ErrorHandlerCmds(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
+        cmd_prefix = self.client.command_prefix(self.client, ctx)
+
         if hasattr(ctx.command, "on_error"):
             return
 
         elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(f"Missing required argument(s). Please search this command with {self.client.command_prefix}help and check which arguments are required.")
+            await ctx.send(f"Missing required argument(s). Please search this command with {cmd_prefix}help and check which arguments are required.")
 
         elif isinstance(error, commands.CommandNotFound):
-            await ctx.send(f"That command is not available. You can check all available commands with {self.client.command_prefix}help.")
+            await ctx.send(f"That command is not available. You can check all available commands with {cmd_prefix}help.")
 
         elif isinstance(error, commands.NoPrivateMessage):
             await ctx.send("This command can not be used in private messages. Please head over to a discord server.")
