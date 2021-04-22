98e50b3d01311984d3c3c0c844d5eaef6deb2148
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-04-22 18:07:58 +0200

    add money to gambling commands

--- cogs/cmds/gambling_cmds.py
+++ cogs/cmds/gambling_cmds.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 
+from cogs.cmds.ecosystem_cmds import Money
+
 class GamblingCmds(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.money = Money()
