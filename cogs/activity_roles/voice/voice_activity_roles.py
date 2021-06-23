106691c50583dba5f21a42362921bdf6696be14f
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-06-23 10:31:06 +0200

    testing vc events

--- cogs/activity_roles/voice/voice_activity_roles.py
+++ cogs/activity_roles/voice/voice_activity_roles.py
@@ -5,4 +5,13 @@ class VcActivityRoles(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.Cog.listener()
+    async def on_voice_state_update(before, after):
+        if before.voice.voice_channel is None and after.voice.voice_channel is not None:
+            try:
+                print("join")
+            except:
+                print("join error")
+
+
     
