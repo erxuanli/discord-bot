6d2f1ca53300ec1bec5137773f5dfeb8504ad15e
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-06-27 13:17:47 +0200

    stats

--- cogs/activity_roles/voice/voice_activity_roles.py
+++ cogs/activity_roles/voice/voice_activity_roles.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 import json
 import os
@@ -8,32 +8,92 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson import json_util
 
+import time
+
 class VcActivityRoles(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.udpate_stats_json()
+        self.editing_json = False
+        self.loaded = False
+        self.database_id = "60d2fce20a8eed87da7c9f79"
+        self.sync_stats_json()
+        
 
     @commands.Cog.listener()
-    async def on_voice_state_update(self, member, before, after):
-        if before.channel is None and after.channel is not None:
-            print(f"hi {member.name} / {member.id}")
-            print(dir(member))
-            print(member.guild)
-            print("\n\n")
-            print(dir(before))
-            print("\n\n")
-            print(dir(after))
-            print("\n\n")
+    async def on_voice_state_update(self, member, before, after):    
+        stats = None
+        self.editing_json = True
+        with open("user_voice_stats.json", "r") as file: # loading existing stats
+            stats = json.loads(json.load(file))
+        with open("user_voice_stats.json", "w") as file: # writing new stats
+            if before.channel is None and after.channel is not None: # user joining vc
+                stats = self.update_stats_json_join(stats.copy(), member)
+            elif before.channel is not None and after.channel is None: # user leaving vc
+                stats = self.update_stats_json_leave(stats.copy(), member)
+            json.dump(json_util.dumps(stats), file)
+        self.editing_json = False
 
-    def udpate_stats_json(self):
+    @tasks.loop(seconds=5)
+    async def upload_json_to_database(self):
+        if self.editing_json or not self.loaded: 
+            pass
+        else:
+            db_client = MongoClient(os.environ['MONGODB'])
+            with db_client:
+                db = db_client["activity"]
+                stats_collection = db["user_voice"]
+                stats = stats_collection.find_one(
+                    ObjectId(self.database_id))
+                with open("user_voice_stats.json", "r") as file:
+                    json_stats = json.loads(json.load(file))
+                    stats = json_stats
+                stats_collection.update_one({"_id": ObjectId(self.database_id)}, {
+                                            "$set": stats}, upsert=True)
+            
+
+
+    def update_stats_json_join(self, dic, member) -> dict:
+        if str(member.guild.id) not in dic: # new server
+            dic[str(member.guild.id)] = {str(member.id) : {"jlvc" : [[time.time()]]}}
+            return dic
+        elif str(member.id) not in dic: # new member
+            dic[str(member.guild.id)][str(member.id)] = {"jlvc" : [[time.time()]]}
+        else:
+            if len(dic[str(member.guild.id)][str(member.id)]["jlvc"][-1]) <= 1: # one or less time stamp
+                dic[str(member.guild.id)][str(member.id)]["jlvc"][-1] = [time.time()]
+            elif len(dic[str(member.guild.id)][str(member.id)]["jlvc"][-1]) == 2:
+                dic[str(member.guild.id)][str(member.id)]["jlvc"].append([time.time()])
+            else:
+                dic[str(member.guild.id)][str(member.id)]["jlvc"].pop(-1)
+                dic[str(member.guild.id)][str(member.id)]["jlvc"].append([time.time()])
+
+
+    def update_stats_json_leave(self, dic, member) -> dict:
+        if str(member.guild.id) not in dic: # new server --> do nothing
+            return dic 
+        elif str(member.id) not in dic: # new member --> do nothing
+            return dic
+        else:
+            if len(dic[str(member.guild.id)][str(member.id)]["jlvc"][-1]) == 0:
+                return dic
+            elif len(dic[str(member.guild.id)][str(member.id)]["jlvc"][-1]) == 1:
+                dic[str(member.guild.id)][str(member.id)]["jlvc"][-1].append(time.time())
+            else:
+                return dic
+
+                      
+
+    def sync_stats_json(self):
         db_client = MongoClient(os.environ['MONGODB'])
         with db_client:
             db = db_client["activity"]
-            prefix_collection = db["user_voice"]
-            stats = prefix_collection.find_one(
-                ObjectId("60d2fce20a8eed87da7c9f79"))
+            stats_collection = db["user_voice"]
+            stats = stats_collection.find_one(
+                ObjectId(self.database_id))
             with open("user_voice_stats.json", "w") as file:
                 json.dump(json_util.dumps(stats), file)
+        self.loaded = True
+
             
 
     
