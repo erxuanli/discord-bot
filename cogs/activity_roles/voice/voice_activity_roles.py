295425577b775cdd3603f1f79be9ace6f180b68a
PolarBear4u <70075936+PolarBear4u@users.noreply.github.com>
2021-06-28 11:16:59 +0200

    vcstats cmd output format

--- cogs/activity_roles/voice/voice_activity_roles.py
+++ cogs/activity_roles/voice/voice_activity_roles.py
@@ -40,8 +40,8 @@ class VcActivityRoles(commands.Cog):
         fields = [(f"VC All Time [{ctx.guild}]", f"{vcat_hours} hours, {vcat_minutes} minutes, {vcat_seconds} seconds", False),
                   ("VC All Time [Global]", f"{vcatg_hours} hours, {vcatg_minutes} minutes, {vcatg_seconds} seconds", False),
                   (f"VC Joins All Time [{ctx.guild}]", str(self.user_all_time_joins(str(ctx.guild.id), str(user.id))), False),
-                  ("VC Joins All Time [Global]", str(self.user_all_time_joins_global(str(user.id))), False),
                   (f"VC Leaves All Time [{ctx.guild}]", str(self.user_all_time_leaves(str(ctx.guild.id), str(user.id))), False),
+                  ("VC Joins All Time [Global]", str(self.user_all_time_joins_global(str(user.id))), False),
                   ("VC Leaves All Time [Global]", str(self.user_all_time_leaves_global(str(user.id))), False)
                   ]
 
