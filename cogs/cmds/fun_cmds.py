37813b11bc8d76f5391aced2825067b0e45b9876
Frozen0wl <77006798+Frozen0wl@users.noreply.github.com>
2021-07-15 20:27:37 +0200

    right bugfix

--- cogs/cmds/fun_cmds.py
+++ cogs/cmds/fun_cmds.py
@@ -19,12 +19,13 @@ class FunCmds(commands.Cog):
     @commands.command()
     async def right(self, ctx):
         k = random.randint(0, 1)
-        pos_vocabulary = [["Absolutely " + ctx.author],["Yes " + ctx.author + ". You are right!"], ["Dead-on!"], ["Precisely!"]]
-        neg_vocabulary = [["No, Screw you!"], ["Ahahahahah, you stupid " + ctx.author +"?"], ["I’m trying my absolute hardest to see things from your perspective, but I just can’t get my head that far up my a*s"], ["Sometimes it’s better to keep your mouth shut and give the impression that you’re stupid than open it and remove all doubt"], ["Everyone’s entitled to act stupid once in awhile, but you really abuse the privilege."] ]
+        member = ctx.author
+        pos_vocabulary = [["Absolutely " + member],["Yes " + member + ". You are right!"], ["Dead-on!"], ["Precisely!"]]
+        neg_vocabulary = [["No, Screw you!"], ["Ahahahahah, you stupid " + member +"?"], ["I’m trying my absolute hardest to see things from your perspective, but I just can’t get my head that far up my a*s"], ["Sometimes it’s better to keep your mouth shut and give the impression that you’re stupid than open it and remove all doubt"], ["Everyone’s entitled to act stupid once in awhile, but you really abuse the privilege."] ]
         pos = random.randint(0, len(pos_vocabulary) - 1)
         neg = random.randint(0, len(neg_vocabulary) - 1)
         if is_moderator():
-            await ctx.send(f"Yes {ctx.author}, you are always right!")
+            await ctx.send(f"Yes {member}, you are always right!")
         elif k == 0:
             await ctx.send(pos_vocabulary[pos])
         else:
