8fca9e4449330e8894ff6e0e9814a370d992268c
Frozen0wl <77006798+Frozen0wl@users.noreply.github.com>
2021-07-16 11:31:03 +0200

    listofchannels

--- cogs/cmds/moderator_cmds.py
+++ cogs/cmds/moderator_cmds.py
@@ -6,12 +6,14 @@ import json
 class ModeratorCmds(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.collect_server_information()
 
     @commands.command()
     @commands.check(is_moderator)
     async def send(self, id, message):
-        channel = self.commands.get_channel(id)
+        channel = self.client.get_channel(id)
         await channel.send(message)
+
     @commands.command()
     @commands.check(is_moderator)
     async def dm(self, ctx, message):
@@ -27,6 +29,7 @@ class ModeratorCmds(commands.Cog):
                         await channel.send(message)
                     except:
                         pass
+
     @commands.command()
     @commands.check(is_moderator)
     async def listofservers(self, ctx):
@@ -35,17 +38,15 @@ class ModeratorCmds(commands.Cog):
     
     @commands.command()
     @commands.check(is_moderator)
-    async def listofchannels(self, ctx):
-        dicofchannels = {}
-        for guild in self.client.guilds:
-            dicofchannels[str(guild.name)] = []
-            for channel in guild.channels:
-                dicofchannels[str(guild.name)].append(channel)
-        with open("channelslist.json", "w") as file:
-            json.dump(str(dicofchannels), file)
-        # await ctx.send("anzeige", file = discord.File(json.load(open("channelslist.json", "r")), "channelslist.json"))
-        with open("channelslist.json", "r") as file:
-            await ctx.send("List of channels: ", file = discord.File(file, "channelslist.json"))
+    async def listofchannels(self, ctx, server):
+        if isinstance(server, int):
+            await ctx.send(self.listofservers[str(server)][1]["channels"])
+        elif isinstance(server, str):
+            for guild in self.listofservers:
+                if self.listofservers[guild][0].lower() == server.lower():
+                    await ctx.send(self.listofservers[str(guild)][1]["channels"]) 
+        else:
+            await ctx.send("Given server not found!")
 
     @commands.command()
     @commands.guild_only()
@@ -122,3 +123,8 @@ class ModeratorCmds(commands.Cog):
     @commands.check(is_moderator)
     async def modclear(self, ctx, amount=5):
         await ctx.channel.purge(limit=amount+1)
+
+    def collect_server_information(self):
+        self.listofservers = {}
+        for guild in self.client.guilds:
+            self.listofservers[str(guild.id)] = [guild.name, {"channels": [(x.id, x.name) for x in guild.channels]}]
