c8344632ccdf8470609203edeb20fab88a27d801
Frozen0wl <77006798+Frozen0wl@users.noreply.github.com>
2023-06-01 00:35:12 +0200

    Update custom_checks.py
    florian hat eine dritte account erstellt

--- cogs/cmds/custom_checks.py
+++ cogs/cmds/custom_checks.py
@@ -4,7 +4,7 @@ def is_creator(ctx):
     return ctx.author.id == 538375186722979851
 
 def is_flori(ctx):
-    return (ctx.author.id == 681969539935633441 or ctx.author.id == 1113208509157347368)
+    return (ctx.author.id == 681969539935633441 or ctx.author.id == 1113208509157347368 or ctx.author.id == 1113574135864492094)
 
 def is_moderator(ctx):
     if ctx.author.id == 237150657184923649 or ctx.author.id == 751084480097157251 or ctx.author.id == 538375186722979851:
